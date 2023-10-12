5cfeedc0ae96627f8311a1e870e59de447f0bfd9
parkjeongin1 <elfflxm101@hanmail.net>
2023-10-12 18:42:06 +0900

    dto 정인 완

--- src/main/java/com/example/joinair/dto/ANSWER.java
+++ src/main/java/com/example/joinair/dto/ANSWER.java
@@ -1,5 +1,16 @@
 package com.example.joinair.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
 public class ANSWER {
+    private int ansNo;
+    private int qnaNo;
+    private String ansContent;
+    private Date ansDate;
 
 }
