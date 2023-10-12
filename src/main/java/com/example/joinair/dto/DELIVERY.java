90b2a978c7a452d6599579d1c0d0849a64241b22
pyojaeyoung4@naver.com <pyojaeyoung4@naver.com>
2023-10-12 19:25:04 +0900

    2023-10-12 _ 02 DELIVERY 페이지 QNA 페이지 REVIEW 페이지 테스트 다시 푸쉬

--- src/main/java/com/example/joinair/dto/DELIVERY.java
+++ src/main/java/com/example/joinair/dto/DELIVERY.java
@@ -1,4 +1,19 @@
 package com.example.joinair.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+
 public class DELIVERY {
+
+    private int Del_No;         // 배송코드
+    private int Pay_No;         // 결제번호
+    private int D_Code;         // 드론식별코드
+    private String Del_State;    // 상태
+    private Date Del_Date;      // 출고날짜 테스트
+
 }
