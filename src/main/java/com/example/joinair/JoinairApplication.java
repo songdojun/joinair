d9eaf44deee999ac1b38e53a7b55460b4e244b09
songdojun <nietzsche93@naver.com>
2023-10-12 11:08:17 +0900

    프로젝트 초기화 완료

---
+++ src/main/java/com/example/joinair/JoinairApplication.java
@@ -0,0 +1,13 @@
+package com.example.joinair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoinairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoinairApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/joinair/JoinairApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.joinair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoinairApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
