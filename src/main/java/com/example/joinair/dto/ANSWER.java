eab325e92957400c5ad922a84a98a6342557c399
parkjeongin1 <elfflxm101@hanmail.net>
2023-10-12 19:22:27 +0900

    Merge remote-tracking branch 'origin/master' into parkjeongin
    # Conflicts:
    #	src/main/java/com/example/joinair/dto/ANSWER.java
    #	src/main/resources/application.properties

--- src/main/java/com/example/joinair/dto/ANSWER.java
+++ src/main/java/com/example/joinair/dto/ANSWER.java
@@ -1,4 +1,15 @@
 package com.example.joinair.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Date;
+
+@Getter
+@Setter
 public class ANSWER {
+    private int ans_No;
+    private int qna_No;
+    private String ans_Content;
+    private Date ans_Date;
 }
