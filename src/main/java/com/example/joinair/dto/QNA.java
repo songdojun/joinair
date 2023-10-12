530a8545ef1b05f109e9c43209ca1fd5c71df08f
pyojaeyoung4@naver.com <pyojaeyoung4@naver.com>
2023-10-12 21:53:10 +0900

    2023-10-12 _ 03 (표재영) 테스트 입니다.

--- src/main/java/com/example/joinair/dto/QNA.java
+++ src/main/java/com/example/joinair/dto/QNA.java
@@ -1,16 +1,4 @@
 package com.example.joinair.dto;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Setter
-@Getter
-
 public class QNA {
-    private int Qna_No;             // 상품문의번호
-    private int Pro_Code;           // 상품코드
-    private String User_Id;         // UserID
-    private String Qna_Title;       // 문의제목
-    private String Qna_Content;     // 문의내용 테스트
-
 }
