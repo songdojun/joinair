774a6c2830d0547f6f1fbc4b2ab9afcc6bf354bb
parkjeongin1 <elfflxm101@hanmail.net>
2023-10-12 21:17:52 +0900

    DTO 완료

--- src/main/java/com/example/joinair/dto/NOTICE.java
+++ src/main/java/com/example/joinair/dto/NOTICE.java
@@ -1,4 +1,19 @@
 package com.example.joinair.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+
 public class NOTICE {
+    private int Not_No;          // 공지사항 번호
+    private String Not_Title;    // 공지사항 제목
+    private String Not_Content;  // 공지사항 내용
+    private Date Not_Date;       // 공지사항 날짜
+    private String Not_Writer;   // 작성자
+
+
 }
