09e9ca90842998c1cfde7cd9e6d166e15f9f0e71
wlsdjrjaxn23@naver.com <@as970505>
2023-10-12 19:03:38 +0900

    커밋 드론 공지사항

--- src/main/java/com/example/joinair/dto/DRONE.java
+++ src/main/java/com/example/joinair/dto/DRONE.java
@@ -1,4 +1,19 @@
 package com.example.joinair.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class DRONE {
+
+    private int D_Code;         //드론식별코드
+    private int D_Payload;      // 페이로드 용랑
+    private int D_Count;            // 수량
+    private int D_Size;             //크기
+    private int D_Weight;           //무게
+    private int D_Speed;           //속도
+
+
+
 }
