286e367d01ada547f4c07b75a990541956271775
pyojaeyoung4@naver.com <pyojaeyoung4@naver.com>
2023-10-12 21:53:10 +0900

    2023-10-12 _ 03 (표재영) 테스트 입니다.

--- src/main/java/com/example/joinair/dto/DELIVERY.java
+++ src/main/java/com/example/joinair/dto/DELIVERY.java
@@ -1,20 +1,4 @@
 package com.example.joinair.dto;
 
-import lombok.Getter;
-import lombok.Setter;
-
-import java.util.Date;
-
-@Getter
-@Setter
-
 public class DELIVERY {
-
-    private int Del_No;         // 배송코드
-    private int Pay_No;         // 결제번호
-    private int D_Code;         // 드론식별코드
-    private String Del_State;    // 상태
-    private Date Del_Date;      // 출고날짜 테스트
-
 }
-//테스
