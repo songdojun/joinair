7a9b772f2f642b89d5e7a03830e89ba208c0c4db
Leodohyun <prada5144@gmail.com>
2023-10-12 19:22:11 +0900

    USERS dto setting

--- src/main/java/com/example/joinair/dto/DRONE_PATH.java
+++ src/main/java/com/example/joinair/dto/DRONE_PATH.java
@@ -1,4 +1,14 @@
 package com.example.joinair.dto;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class DRONE_PATH {
+    private Long dpCode; // 드론경로코드
+    private Long dCode; // 드론식별코드
+    private String dpPlace; // 도착장소
+    private String dpInfo; // 경로정보
+    private String dpStime; // 소요시간
+    private String dpSplace; // 출발장소
 }
