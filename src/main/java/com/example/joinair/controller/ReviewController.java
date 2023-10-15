e803ceff0eb71018ceb6cdd54cab9918bd88494d
songdojun <nietzsche93@naver.com>
2023-10-15 15:16:46 +0900

    준혁branch- 리뷰 게시판 수정 , 삭제기능 까지 구현

--- src/main/java/com/example/joinair/controller/ReviewController.java
+++ src/main/java/com/example/joinair/controller/ReviewController.java
@@ -5,7 +5,9 @@ import com.example.joinair.entity.Review;
 import com.example.joinair.service.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import static com.example.joinair.service.ReviewService.write;
@@ -26,8 +28,40 @@ public class ReviewController {
 
         write(review);
 
-        return "";
+        return "redirect:/review/list";
 
     }
+
+    @GetMapping("/review/list")
+    public String reviewList(Model model){
+
+       model.addAttribute("list",reviewService.reviewList());
+        
+        return "reviewlist";
+    }
+
+
+    @GetMapping("/review/view")
+    public String reviewView(Model model, Integer Rev_No) {
+        model.addAttribute("Review", reviewService.reviewView(Rev_No).orElse(null));
+        return "reviewview";
+    }
+
+
+    @GetMapping("/review/delete")
+    public String boardDelete(Integer Rev_No){
+        reviewService.reviewDelete(Rev_No);
+
+        return "redirect:/review/list";
+    }
+
+    @GetMapping("/review/modify/{Rev_No}")
+    public String reviewModify(@PathVariable("Rev_No") Integer Rev_No,Model model){
+
+        model.addAttribute("review", reviewService.reviewView(Rev_No));
+
+        return "reviewmodify";
+    }
+
 
 }
--- src/main/java/com/example/joinair/service/ReviewService.java
+++ src/main/java/com/example/joinair/service/ReviewService.java
@@ -5,6 +5,9 @@ import com.example.joinair.repository.ReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class ReviewService {
 
@@ -15,7 +18,30 @@ public class ReviewService {
         this.reviewRepository = reviewRepository;
     }
 
+
+    //글 작성 처리
     public static void write(Review review) {
         reviewRepository.save(review);
     }
+
+
+    //게시글 리스트 처리
+    public List<Review> reviewList(){
+
+        return reviewRepository.findAll();
+    }
+
+    //특정 게시글 불러오기
+
+    public Optional<Review> reviewView(Integer Rev_No){
+        return Optional.of(reviewRepository.findById(Rev_No).get());
+    }
+
+
+    //특정 게시글 삭제
+    public void reviewDelete(Integer Rev_No){
+
+        reviewRepository.deleteById(Rev_No);
+    }
+
 }
