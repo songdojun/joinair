c8f11f8981d12aab5804b9752ad2f9e4df7ef9b1
songdojun <nietzsche93@naver.com>
2023-10-26 10:38:23 +0900

    TestMergeProject branch-(준혁수정) -성현 브렌치 병합 완료

--- src/main/java/com/example/joinair/dto/QNA.java
+++ src/main/java/com/example/joinair/dto/QNA.java
@@ -6,6 +6,70 @@ import lombok.Setter;
 @Setter
 @Getter
 public class QNA {
+    public int getQNA_NO() {
+        return QNA_NO;
+    }
+
+    public void setQNA_NO(int QNA_NO) {
+        this.QNA_NO = QNA_NO;
+    }
+
+    public int getPRO_CODE() {
+        return PRO_CODE;
+    }
+
+    public void setPRO_CODE(int PRO_CODE) {
+        this.PRO_CODE = PRO_CODE;
+    }
+
+    public String getUSER_ID() {
+        return USER_ID;
+    }
+
+    public void setUSER_ID(String USER_ID) {
+        this.USER_ID = USER_ID;
+    }
+
+    public String getQNA_TITLE() {
+        return QNA_TITLE;
+    }
+
+    public void setQNA_TITLE(String QNA_TITLE) {
+        this.QNA_TITLE = QNA_TITLE;
+    }
+
+    public String getQNA_CONTENT() {
+        return QNA_CONTENT;
+    }
+
+    public void setQNA_CONTENT(String QNA_CONTENT) {
+        this.QNA_CONTENT = QNA_CONTENT;
+    }
+
+    public String getQNA_COMMAND() {
+        return QNA_COMMAND;
+    }
+
+    public void setQNA_COMMAND(String QNA_COMMAND) {
+        this.QNA_COMMAND = QNA_COMMAND;
+    }
+
+    public Boolean getFlag() {
+        return flag;
+    }
+
+    public void setFlag(Boolean flag) {
+        this.flag = flag;
+    }
+
+    public String getKeyword() {
+        return keyword;
+    }
+
+    public void setKeyword(String keyword) {
+        this.keyword = keyword;
+    }
+
     private int QNA_NO;           // 상품문의번호
     private int PRO_CODE;         // 상품코드
     private String USER_ID;       // UserID
--- src/main/java/com/example/joinair/dto/QNAPAGE.java
+++ src/main/java/com/example/joinair/dto/QNAPAGE.java
@@ -6,6 +6,22 @@ import lombok.Setter;
 @Setter
 @Getter
 public class QNAPAGE {
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
     private int page; // 현재 페이지
     private int pageSize; // 페이지당 항목 수
 }
