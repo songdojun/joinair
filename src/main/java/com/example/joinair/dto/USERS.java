8b8a5fb9a41485e3ec99caed53a617b1735b2c1b
songdojun <nietzsche93@naver.com>
2023-10-26 10:13:58 +0900

    TestMergeProject branch-(준혁수정) - 도현 브렌치 병합 작업완료

--- src/main/java/com/example/joinair/dto/USERS.java
+++ src/main/java/com/example/joinair/dto/USERS.java
@@ -7,6 +7,78 @@ import lombok.Setter;
 @Setter
 public class USERS {
 
+    public String getUser_Id() {
+        return User_Id;
+    }
+
+    public void setUser_Id(String user_Id) {
+        User_Id = user_Id;
+    }
+
+    public String getUser_Name() {
+        return User_Name;
+    }
+
+    public void setUser_Name(String user_Name) {
+        User_Name = user_Name;
+    }
+
+    public String getUser_Email() {
+        return User_Email;
+    }
+
+    public void setUser_Email(String user_Email) {
+        User_Email = user_Email;
+    }
+
+    public String getUser_Password() {
+        return User_Password;
+    }
+
+    public void setUser_Password(String user_Password) {
+        User_Password = user_Password;
+    }
+
+    public String getUser_Address() {
+        return User_Address;
+    }
+
+    public void setUser_Address(String user_Address) {
+        User_Address = user_Address;
+    }
+
+    public String getUser_Phone() {
+        return User_Phone;
+    }
+
+    public void setUser_Phone(String user_Phone) {
+        User_Phone = user_Phone;
+    }
+
+    public int getUser_Mileage() {
+        return User_Mileage;
+    }
+
+    public void setUser_Mileage(int user_Mileage) {
+        User_Mileage = user_Mileage;
+    }
+
+    public String getUser_DetailAddress() {
+        return User_DetailAddress;
+    }
+
+    public void setUser_DetailAddress(String user_DetailAddress) {
+        User_DetailAddress = user_DetailAddress;
+    }
+
+    public String getUser_Mode() {
+        return User_Mode;
+    }
+
+    public void setUser_Mode(String user_Mode) {
+        User_Mode = user_Mode;
+    }
+
     private String User_Id;
     private String User_Name;
     private String User_Email;
--- src/main/java/com/example/joinair/entity/Users.java
+++ src/main/java/com/example/joinair/entity/Users.java
@@ -1,23 +1,87 @@
-package com.example.joinair.entity;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-
-
-//User entity
-@Entity
-@Table(name = "USERS") // USERS 테이블과 매핑
-public class Users {
-
-    @Id
-    private String User_Id;
-    private String User_Name;
-    private String User_Email;
-    private String User_Password;
-    private String User_Address;
-    private String User_Phone;
-    private int User_Mileage;
-    private String User_DetailAddress; // 추가: 상세 주소 필드
-    // 다른 필드, Getter, Setter 및 설정
-}
+//package com.example.joinair.entity;
+//
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.Id;
+//import jakarta.persistence.Table;
+//
+//
+////User entity
+//@Entity
+//@Table(name = "USERS") // USERS 테이블과 매핑
+//public class Users {
+//
+//    public String getUser_Id() {
+//        return User_Id;
+//    }
+//
+//    public void setUser_Id(String user_Id) {
+//        User_Id = user_Id;
+//    }
+//
+//    public String getUser_Name() {
+//        return User_Name;
+//    }
+//
+//    public void setUser_Name(String user_Name) {
+//        User_Name = user_Name;
+//    }
+//
+//    public String getUser_Email() {
+//        return User_Email;
+//    }
+//
+//    public void setUser_Email(String user_Email) {
+//        User_Email = user_Email;
+//    }
+//
+//    public String getUser_Password() {
+//        return User_Password;
+//    }
+//
+//    public void setUser_Password(String user_Password) {
+//        User_Password = user_Password;
+//    }
+//
+//    public String getUser_Address() {
+//        return User_Address;
+//    }
+//
+//    public void setUser_Address(String user_Address) {
+//        User_Address = user_Address;
+//    }
+//
+//    public String getUser_Phone() {
+//        return User_Phone;
+//    }
+//
+//    public void setUser_Phone(String user_Phone) {
+//        User_Phone = user_Phone;
+//    }
+//
+//    public int getUser_Mileage() {
+//        return User_Mileage;
+//    }
+//
+//    public void setUser_Mileage(int user_Mileage) {
+//        User_Mileage = user_Mileage;
+//    }
+//
+//    public String getUser_DetailAddress() {
+//        return User_DetailAddress;
+//    }
+//
+//    public void setUser_DetailAddress(String user_DetailAddress) {
+//        User_DetailAddress = user_DetailAddress;
+//    }
+//
+//    @Id
+//    private String User_Id;
+//    private String User_Name;
+//    private String User_Email;
+//    private String User_Password;
+//    private String User_Address;
+//    private String User_Phone;
+//    private int User_Mileage;
+//    private String User_DetailAddress; // 추가: 상세 주소 필드
+//    // 다른 필드, Getter, Setter 및 설정
+//}
