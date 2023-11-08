a7b59aa4fa7932a2fdd5473d2add7f7457c1922b
wlsdjrjaxn23@naver.com <@as970505>
2023-11-08 18:30:21 +0900

    소성현 - 11/08 까지 푸쉬

--- src/main/java/com/example/joinair/dto/ORDERS.java
+++ src/main/java/com/example/joinair/dto/ORDERS.java
@@ -6,12 +6,12 @@ import lombok.Setter;
 @Getter
 @Setter
 public class ORDERS {
-    private int Orders_Num;         // 주문번호
-    private String User_Id;          // UserID
-    private int Pro_Code;           // 상품코드
-    private int Orders_Total_Price; // 총 주문금액
-    private String Orders_State;     // 상태
-    private int Orders_Total;    // 총주문수량
+        private int Orders_Num;         // 주문번호
+        private String User_Id;          // UserID
+        private int Pro_Code;           // 상품코드
+        private int Orders_Total_Price; // 총 주문금액
+        private String Orders_State;     // 상태
+        private int Orders_Total;    // 총주문수량
 
 
 
