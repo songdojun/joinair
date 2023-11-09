d5827998039917f5348cd02ca6e0ff27798af63d
songdojun <nietzsche93@naver.com>
2023-11-09 18:57:03 +0900

    TestMergeProject(준혁올림) : ORDER_DETAIL 주석 추가함

--- src/main/java/com/example/joinair/dto/ORDER_DETAIL.java
+++ src/main/java/com/example/joinair/dto/ORDER_DETAIL.java
@@ -8,13 +8,13 @@ import java.util.Date;
 @Getter
 @Setter
 public class ORDER_DETAIL {
-    private Long OD_Num; // 주문상세번호
-    private Long Orders_Num; // 주문번호
-    private String OD_Request; // 배송요청사항
-    private Date OD_Date; // 주문날짜
-    private int OD_Qua; // 수량
-    private double OD_Total_Price; // 총 소계금액
-    private double OD_Totalweight; // 총 무게
-    private String OD_Location; // 위치
-    private Date OD_Reqtime; // 요청배달시간
+    private Long OD_Num; // 주문상세번호    (ORDER_DETAIL의 기본키, 자동1증가생성 시퀀스 만들어줘야 하고 DB에 적용해줘야함 . )
+    private Long Orders_Num; // 주문번호    (ORDER_DETAIL의 외래키)
+    private String OD_Request; // 배송요청사항   (X)
+    private Date OD_Date; // 주문날짜          (X)
+    private int OD_Qua; // 수량             (session에 담긴 Quantity)
+    private double OD_Total_Price; // 총 소계금액  (session에 담긴 Subtotal)
+    private double OD_Totalweight; // 총 무게     (session에 담긴 weight)
+    private String OD_Location; // 위치             (x)
+    private Date OD_Reqtime; // 요청배달시간         (X)
 }
