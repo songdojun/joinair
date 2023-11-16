f154342d49b36b28bb392108b02be3209ebaaa24
oosh70 <oosh70@gmail.com>
2023-11-16 16:01:47 +0900

    서버

--- src/main/java/com/example/joinair/controller/OrderController.java
+++ src/main/java/com/example/joinair/controller/OrderController.java
@@ -9,10 +9,7 @@ package com.example.joinair.controller;
         import org.springframework.beans.factory.annotation.Autowired;
         import org.springframework.stereotype.Controller;
         import org.springframework.ui.Model;
-        import org.springframework.web.bind.annotation.GetMapping;
-        import org.springframework.web.bind.annotation.PathVariable;
-        import org.springframework.web.bind.annotation.PostMapping;
-        import org.springframework.web.bind.annotation.RequestMapping;
+        import org.springframework.web.bind.annotation.*;
 
         import java.util.List;
 
@@ -31,6 +28,7 @@ public class OrderController {
     }
 
     @PostMapping("/place")
+    @ResponseBody
     public String placeOrder(HttpSession session, Model model) {
         // 세션에서 카트 정보를 가져와서 ORDER_DETAIL 객체로 변환하여 DB에 저장
         orderService.placeOrderFromCart(session);
@@ -49,11 +47,13 @@ public class OrderController {
             System.out.println("Failed to retrieve orderId from session.");
         }
         // 리다이렉트 없이 현재 페이지에서 유지 => 콘솔창에서는 에러 뜸 /
-        return "redirect:/order/orderConfirm/" + orderId;
+        return String.valueOf(orderId);
+
+
     }
     @GetMapping("/orderConfirm/{orderId}")
     public String showOrderConfirmPage(@PathVariable Integer orderId, Model model) {
-        // orderId를 이용하여 주문 정보를 가져오는 메서드 호출
+        System.out.println("test");
         ORDERS order = orderService.getOrder(orderId);
         logger.info("Received orderId: {}", orderId);
         List<ORDER_DETAIL> orderDetails = orderService.getOrderDetails(orderId);
