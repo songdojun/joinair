917698c9f2648fc5c633314430f685af9ee99856
Leodohyun <prada5144@gmail.com>
2023-11-19 21:30:47 +0900

    1119 21:29 index page 모달창에서 '주문 완료 및 결제 하기' 버튼 클릭시 차감된 변경 마일리지 Users테이블에 Update.

--- src/main/java/com/example/joinair/controller/OrderController.java
+++ src/main/java/com/example/joinair/controller/OrderController.java
@@ -4,6 +4,7 @@ package com.example.joinair.controller;
         import com.example.joinair.dto.ORDER_DETAIL;
         import com.example.joinair.dto.USERS;
         import com.example.joinair.service.OrderService;
+        import com.example.joinair.service.UserService;
         import jakarta.servlet.http.HttpSession;
         import org.slf4j.Logger;
         import org.slf4j.LoggerFactory;
@@ -24,10 +25,12 @@ public class OrderController {
     private static final Logger logger = LoggerFactory.getLogger(OrderController.class);
 
     private final OrderService orderService;
+    private final UserService userService;
 
     @Autowired
-    public OrderController(OrderService orderService) {
+    public OrderController(OrderService orderService, UserService userService) {
         this.orderService = orderService;
+        this.userService = userService;
     }
 
     @PostMapping("/place")
@@ -35,16 +38,21 @@ public class OrderController {
     public String placeOrder(HttpSession session, Model model, String finalAmount,
                              @RequestParam("orderPname") String orderPname,
                              @RequestParam("orderDest1") String orderDest1,
-                             @RequestParam("orderPhone1") String orderPhone1
-                             ) {
+                             @RequestParam("orderPhone1") String orderPhone1,
+                             int afuserMileage) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
 
         // finalAmount = 마일리지 적용 값(문자열 인데 앞에 콤마가 붙어있음 // 서브스트링으로 콤마를 제거
         String total = finalAmount.replace(",", "");
+
         System.out.println("replace total :" + total);
 
         // 세션에서 카트 정보를 가져와서 ORDER_DETAIL 객체로 변환하여 DB에 저장
         orderService.placeOrderFromCart(session, total, orderPname, orderDest1, orderPhone1);
                                                 //total = db저장함수에 마일리지가 적용된 값을 같이 넘겨줌
+        userService.updateUserMileage(username, afuserMileage);
         // 로그 추가
         System.out.println("Order placed successfully!");
 
