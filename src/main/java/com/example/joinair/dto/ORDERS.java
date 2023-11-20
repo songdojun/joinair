fd77ec7e6151d0130f8cb21b91a8112b4116bdf9
songdojun <nietzsche93@naver.com>
2023-11-20 19:24:54 +0900

    TestMergeProject(준혁올림) : dto수정 주문,주문상세

--- src/main/java/com/example/joinair/dto/ORDERS.java
+++ src/main/java/com/example/joinair/dto/ORDERS.java
@@ -3,11 +3,81 @@ package com.example.joinair.dto;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.math.BigDecimal;
-
 @Getter
 @Setter
 public class ORDERS {
+        public int getOrders_Num() {
+                return Orders_Num;
+        }
+
+        public void setOrders_Num(int orders_Num) {
+                Orders_Num = orders_Num;
+        }
+
+        public String getUser_Id() {
+                return User_Id;
+        }
+
+        public void setUser_Id(String user_Id) {
+                User_Id = user_Id;
+        }
+
+        public int getOrders_Total_Price() {
+                return Orders_Total_Price;
+        }
+
+        public void setOrders_Total_Price(int orders_Total_Price) {
+                Orders_Total_Price = orders_Total_Price;
+        }
+
+        public int getOrders_Totalweight() {
+                return Orders_Totalweight;
+        }
+
+        public void setOrders_Totalweight(int orders_Totalweight) {
+                Orders_Totalweight = orders_Totalweight;
+        }
+
+        public String getPay_Method() {
+                return Pay_Method;
+        }
+
+        public void setPay_Method(String pay_Method) {
+                Pay_Method = pay_Method;
+        }
+
+        public String getOrders_Pname() {
+                return Orders_Pname;
+        }
+
+        public void setOrders_Pname(String orders_Pname) {
+                Orders_Pname = orders_Pname;
+        }
+
+        public String getOrders_Dest() {
+                return Orders_Dest;
+        }
+
+        public void setOrders_Dest(String orders_Dest) {
+                Orders_Dest = orders_Dest;
+        }
+
+        public String getOrders_Phone() {
+                return Orders_Phone;
+        }
+
+        public void setOrders_Phone(String orders_Phone) {
+                Orders_Phone = orders_Phone;
+        }
+
+        public int getDel_No() {
+                return Del_No;
+        }
+
+        public void setDel_No(int del_No) {
+                Del_No = del_No;
+        }
+
         private int Orders_Num;         // 주문번호 (1씩 자동증가 생성하는 시퀀스 만들어줘야함 DB)
 
         private String User_Id;          // UserID(생성함)
--- src/main/java/com/example/joinair/dto/ORDER_DETAIL.java
+++ src/main/java/com/example/joinair/dto/ORDER_DETAIL.java
@@ -8,6 +8,94 @@ import java.util.Date;
 @Getter
 @Setter
 public class ORDER_DETAIL {
+    public int getOD_Num() {
+        return OD_Num;
+    }
+
+    public void setOD_Num(int OD_Num) {
+        this.OD_Num = OD_Num;
+    }
+
+    public int getOrders_Num() {
+        return Orders_Num;
+    }
+
+    public void setOrders_Num(int orders_Num) {
+        Orders_Num = orders_Num;
+    }
+
+    public String getOD_Pro_Name() {
+        return OD_Pro_Name;
+    }
+
+    public void setOD_Pro_Name(String OD_Pro_Name) {
+        this.OD_Pro_Name = OD_Pro_Name;
+    }
+
+    public int getOD_Pro_Price() {
+        return OD_Pro_Price;
+    }
+
+    public void setOD_Pro_Price(int OD_Pro_Price) {
+        this.OD_Pro_Price = OD_Pro_Price;
+    }
+
+    public int getOD_Qua() {
+        return OD_Qua;
+    }
+
+    public void setOD_Qua(int OD_Qua) {
+        this.OD_Qua = OD_Qua;
+    }
+
+    public int getOD_Totalweight() {
+        return OD_Totalweight;
+    }
+
+    public void setOD_Totalweight(int OD_Totalweight) {
+        this.OD_Totalweight = OD_Totalweight;
+    }
+
+    public int getOD_Total_Price() {
+        return OD_Total_Price;
+    }
+
+    public void setOD_Total_Price(int OD_Total_Price) {
+        this.OD_Total_Price = OD_Total_Price;
+    }
+
+    public String getOD_Location() {
+        return OD_Location;
+    }
+
+    public void setOD_Location(String OD_Location) {
+        this.OD_Location = OD_Location;
+    }
+
+    public Date getOD_Reqtime() {
+        return OD_Reqtime;
+    }
+
+    public void setOD_Reqtime(Date OD_Reqtime) {
+        this.OD_Reqtime = OD_Reqtime;
+    }
+
+    public String getOD_Request() {
+        return OD_Request;
+    }
+
+    public void setOD_Request(String OD_Request) {
+        this.OD_Request = OD_Request;
+    }
+
+    public Date getOD_Date() {
+        return OD_Date;
+    }
+
+    public void setOD_Date(Date OD_Date) {
+        this.OD_Date = OD_Date;
+    }
+
     private int OD_Num; // 주문상세번호    (ORDER_DETAIL의 기본키, 자동1증가생성 시퀀스 만들어줘야 하고 DB에 적용해줘야함 . )
     private int Orders_Num; // 주문번호    (x)
     private String OD_Pro_Name; // 주문한 상품명    ( DB컬럼 생성 필요함)
