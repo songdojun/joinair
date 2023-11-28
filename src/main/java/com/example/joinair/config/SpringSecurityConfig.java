e3a8646010ba47bfc35efb24c91fb0f998390670
songdojun <nietzsche93@naver.com>
2023-11-28 12:13:08 +0900

    TestMergeProject(준혁올림) : 성현공지사항 접근 수정

--- src/main/java/com/example/joinair/config/SpringSecurityConfig.java
+++ src/main/java/com/example/joinair/config/SpringSecurityConfig.java
@@ -2,27 +2,20 @@
 
 package com.example.joinair.config;
 
-import jakarta.servlet.DispatcherType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
-import static org.springframework.security.config.Customizer.withDefaults;
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 
@@ -73,7 +66,9 @@ public class SpringSecurityConfig {
                                 .requestMatchers(antMatcher("/adminEditUserList")).hasAuthority("admin")
                                 .requestMatchers(antMatcher("/productad/**")).hasAuthority("admin")
                                 .requestMatchers(antMatcher("/dronead/**")).hasAuthority("admin")
+                                .requestMatchers(antMatcher("/notice/create/**")).hasAuthority("admin")
                                 .anyRequest().permitAll()
+
                 )
 
                 .headers(
